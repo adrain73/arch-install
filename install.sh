a15cf3795f7f47bd91694c7d2491c3cc59105fd7
Adraelsen <adrianrayishmael@gmail.com>
2020-08-18 14:37:39 -0500

    Added script for getting password form user

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 hostname=$(dialog --stdout --inputbox "Enter hostname" 0 0) || exit 1
 : ${hostname:?"hostname cannot be empty"}
+
+psswd1=$(dialog --stdout --passwordbox "Enter password" 0 0) || exit 1
+psswd2=$(dialog --stdout --passwordbox "Reenter password" 0 0) || exit 1
+[[ psswd1 == psswd 2 ]] || (echo "Passwords did not match"; exit 1;)
+
 clear
---
+++ install.sh~
@@ -0,0 +1,3 @@
+hostname=$(dialog --stdout --inputbox "Enter hostname" 0 0) || exit 1
+: ${hostname:?"hostname cannot be empty"}
+clear
