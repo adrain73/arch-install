cebeb50911c403745ae7cfd818bf8b43bbad16f6
Adrian Ishmael <49854719+Adraelsen@users.noreply.github.com>
2020-11-10 21:12:17 -0600

    added config to subscript

--- chroot.sh
+++ chroot.sh
@@ -1,1 +1,14 @@
 ### Subscript to run in chroot environemnt
+
+
+### Configure system
+ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime	
+hwclock --systohc
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf 
+echo "$hostname" >> /etc/hostname 
+echo -e "127.0.0.1\t$hostname\n::1\t$hostname\n120.0.1.1\t$hostname.localdomain $hostname"
+mkinitcpio -P
+echo "root:$pass1" | chpasswd
+
+echo -e "Configuration complete.\n"
