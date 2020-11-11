3effef4018865a5e0762e16acb6a1ae6874fafd2
Adrian Ishmael <49854719+Adraelsen@users.noreply.github.com>
2020-11-10 21:12:59 -0600

    added grub install to subscript

--- chroot.sh
+++ chroot.sh
@@ -1,6 +1,5 @@
 ### Subscript to run in chroot environemnt
 
-
 ### Configure system
 ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime	
 hwclock --systohc
@@ -12,3 +11,10 @@ mkinitcpio -P
 echo "root:$pass1" | chpasswd
 
 echo -e "Configuration complete.\n"
+
+### Install bootloader
+echo "Installing bootloader . . ."
+pacman -S grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+echo -e "Install complete.\n"
